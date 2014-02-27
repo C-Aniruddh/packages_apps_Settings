e692dee6dfacf95923aacbd8534a29a48b0d1bd9
Tianrui Guo <tianrui@pac-rom.com>
2014-02-26 23:10:07 -0800

    ADB Paranoid: Dont show pref if RSA verification is disabled
    Change-Id: If01a27aa5f15a59350d9dd2970e7e6d63df0754c
    Signed-off-by: Tianrui Guo <tianrui@pac-rom.com>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -289,6 +289,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         mClearAdbKeys = findPreference(CLEAR_ADB_KEYS);
         if (!SystemProperties.getBoolean("ro.adb.secure", false)) {
             if (debugDebuggingCategory != null) {
+                debugDebuggingCategory.removePreference(mAdbParanoid);
                 debugDebuggingCategory.removePreference(mClearAdbKeys);
             }
         }
